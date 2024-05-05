54fef7d1e03bfecd776df7811b06c04999947062
olopcodes <olopcodes@gmail.com>
2024-05-04 22:34:39 -0400

    working on formatting number with decimals, simple math works

--- js/calculator.js
+++ js/calculator.js
@@ -3,56 +3,77 @@ const calculatorOutputEl = document.querySelector('.calculator__output');
 const calculatorResultEl = document.querySelector('.calculator__result');
 
 const calculator = {
-    number1: [],
-    number2: [],
+    arrNumber1: [],
+    arrNumber2: [],
+    number1: 0,
+    number2: 0,
     operation: '',
     result: 0,
 
+    formatArrToNumber() {
+        this.number1 = Number(this.arrNumber1.join())
+        if(this.arrNumber2.length > 0) {
+            this.number2 = Number(this.arrNumber2.join());
+        } 
+    },
+
     sum() {
-        this.result = +this.number1.join() + this.number2.join();
+       this.result = this.number1 + this.number2;
     },
 
     subtract() {
-        this.result = +this.number1.join() - this.number2.join();
-
+        this.result = this.number1 - this.number2;
     },
 
     divide() {
-        if(+this.number2.join() === 0) {
+        if(this.number2 === 0) {
             console.log('no divinding by 0 silly goose');
             return
         }
 
-        this.result = +this.number1.join() / this.number2.join();
+        this.result = this.number1 / this.number2;
     },
 
     multiply() {
-        this.result = +this.number1.join() * this.number2.join();
+        this.result = this.number1 * this.number2;     
     },
 
     operate() {
-        if(this.operation === '+') {
+        
+        this.formatArrToNumber();
+
+;        if(this.operation === '+') {
             this.sum();
-        } else if (this.operation === 'sub') {
+        } else if (this.operation === '-') {
             this.subtract();
-        } else if (this.operation === 'div') {
+        } else if (this.operation === '/') {
             this.divide();
-        } else if (this.operation === 'mul') {
+        } else if (this.operation === '*') {
             this.multiply();
         }
 
-        // fixing the number values after calculating
-        this.number1 = this.number1.push(this.result);
-        this.number2 = [];
-        this.result = 0;
+        // if number is a decimal, fixed 2 spots
+        this.formatDecimalNumber();
+    },
+
+    formatDecimalNumber() {
+        if(!Number.isInteger(this.number1)) {
+            this.number1 = Number(this.number1.toFixed(2).split(",").join(""));
+        } else if (!Number.isInteger(this.number2)) {
+            this.number2 = Number(this.number1.toFixed(2).split(",").join(""));
+        } else if (!Number(this.result)) {
+            Number(this.result = this.result.toFixed(2).split(",").join(""));
+        }
     },
 
     clearAll() {
         // clearing values
-        calculator.number1 = [];
-        calculator.number2 = [];
-        calculator.operation = '';
-        calculator.result = 0;
+        this.arrNumber1 = [];
+        this.arrNumber2 = [];
+        this.number1 = 0;
+        this.number2 = 0;
+        this.operation = '';
+        this.result = 0;
         calculatorOutputEl.textContent = '';
         calculatorResultEl.textContent = '';
     }
@@ -63,15 +84,21 @@ calculatorBodyEl.addEventListener('click', e => {
     // when number is clicked
     if(e.target.classList.contains('calculator__num')) {
         if(calculator.operation) {
-            calculator.number2.push(e.target.textContent);
+            calculator.arrNumber2.push(e.target.textContent);
         } else {
-            calculator.number1.push(e.target.textContent);
+            calculator.arrNumber1.push(e.target.textContent);
+            console.log(calculator.arrNumber1, 'calc num when clicked')
         }
     } else if (e.target.classList.contains('calculator__operation')) {
         // operation should not work if num1 is empty
-        if(calculator.number1.length === 0) return;
+        if(calculator.arrNumber1.length === 0) return;
         // if number 2 already selected, perform operation after operation is clicked again
-        calculator.operate();
+        if(calculator.arrNumber2.length === 0) {
+            calculator.operation = e.target.textContent;
+        } else {
+            // console.log('add op')
+            calculator.operate(e.target.textContent);
+        }
     } else if (e.target.classList.contains('calculator__clear')) {
         calculator.clearAll();
     } else if (
@@ -80,7 +107,27 @@ calculatorBodyEl.addEventListener('click', e => {
     ) {
         console.log('neg/pos')
     } else if (e.target.classList.contains('calculator__decimal')) {
-        console.log('decimal')
+        if(calculator.arrNumber1.length > 0) {
+            // format arr to a number
+            calculator.formatArrToNumber();
+
+            if(Number.isInteger(calculator.number1)) {
+                calculator.arrNumber1.push('.');
+            } else {
+                console.log('no more decimals');
+                return
+            }
+            
+            if (
+                Number.isInteger(calculator.number2) &&
+                calculator.operation
+            ) {
+                calculator.arrNumber2.push('.');
+            } else {
+                console.log('no more decimals');
+                return
+            }
+        }
     } else if (e.target.classList.contains('calculator__equal')) {
         calculator.operate();
         
